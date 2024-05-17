a936d147eeb3612665c0c7b47b4483f4d9d69d8d
ttsoncs <tson25102002@gmail.com>
2024-05-18 00:21:27 +0700

    .

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -36,7 +36,7 @@ const StyledLink = styled(Link)`
   font-size: 24px;
   font-weight: var(--font-weight-semibold);
 
-  color: var(--color-primary);
+  color: var(--color-primary-text);
   text-decoration: none;
 `;
 
