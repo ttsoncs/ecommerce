4294c029502b00fb8da5dfbbbb9a279e9e845d24
ttsoncs <tson25102002@gmail.com>
2024-05-20 22:07:37 +0700

    .

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,7 +26,7 @@ function Header() {
       <Wrapper>
         <Logo
           handleClose={handleClose}
-          size={16}
+          size={20}
         />
         <Toggle
           isModalOpen={isModalOpen}
