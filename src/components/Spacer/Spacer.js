373afb41b7c69d9f193b40218cfc334f997200c1
ttsoncs <tson25102002@gmail.com>
2024-05-31 19:39:52 +0700

    .

--- src/components/Spacer/Spacer.js
+++ src/components/Spacer/Spacer.js
@@ -3,8 +3,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import { QUERIES } from "@/constants";
-
 function Spacer({ size = 0, hasUnit = false }) {
   const finalSize = hasUnit ? size : `${size}px`;
 
@@ -19,10 +17,6 @@ function Spacer({ size = 0, hasUnit = false }) {
 
 const Space = styled.div`
   height: var(--size);
-
-  @media ${QUERIES.phoneAndSmaller} {
-    height: calc(var(--size) * 0.8);
-  }
 `;
 
 export default Spacer;
